8d42348dd420b1cd5eb7606879bb109b0022a56d
msedkiewicz <magdalenainspirations@gmail.com>
2026-07-03 13:16:20 +0200

    add first test

---
+++ src/components/Greeting.test.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+import { describe, it, expect } from "vitest";
+import "@testing-library/jest-dom/vitest";
+
+describe("Greting", () => {
+  it("renders a default greeting", () => {
+    render(<Greeting />);
+    expect(screen.getByText("Hello, World!")).toBeInTheDocument();
+  });
+
+  it("renders greeting with a name", () => {});
+});
