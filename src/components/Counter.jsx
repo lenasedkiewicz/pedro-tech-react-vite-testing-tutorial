94bda6c1228b421ad0d26a59156c6a6717a2c58b
msedkiewicz <magdalenainspirations@gmail.com>
2026-07-03 16:53:22 +0200

    add useCounter hook

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import { useCounter } from "../hooks/useCounter";
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  const { count, increment, decrement } = useCounter();
   return (
     <div>
       <p data-testid="counter-value">{count}</p>
-      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <button onClick={increment}>Increment</button>
+      <p></p>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
